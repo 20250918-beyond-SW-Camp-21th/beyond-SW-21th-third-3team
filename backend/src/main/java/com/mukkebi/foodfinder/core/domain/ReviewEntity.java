12cc8a10272070fed3b7af28d4e7b21f9d34fae3
이용우 <qhtm008@naver.com>
2025-12-23 11:05:31 +0900

    fix(review):리ë뷰 엔티티 수정

--- backend/src/main/java/com/mukkebi/foodfinder/core/domain/ReviewEntity.java
+++ backend/src/main/java/com/mukkebi/foodfinder/core/domain/ReviewEntity.java
@@ -2,32 +2,40 @@ package com.mukkebi.foodfinder.core.domain;
 
 import com.mukkebi.foodfinder.storage.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(
-        name = "review",
+        name = "reviews",
         indexes = {
             @Index(name="idx_review_user",columnList="user_id")
         }
 )
 public class ReviewEntity extends BaseEntity {
 
-    @Column(nullable = true, length = 1000, name = "content")
+    @Column(length = 1000)
     private String content;
 
-    @Column(nullable=false, name = "rating")
+    @Column(nullable=false)
     private Double rating;
 
     @Column(nullable=false, name = "user_id")
     private Long userId;
 
     @Column(nullable=false, name = "recommendation_id")
-    private Long reconmmendationId;
+    private Long recommendationId;
 
+    protected ReviewEntity(String content, Double rating, Long userId, Long recommendationId) {
+        this.content = content;
+        this.rating = rating;
+        this.userId = userId;
+        this.recommendationId = recommendationId;
+    }
+
+    public static ReviewEntity create(String content, Double rating, Long userId, Long recommendationId) {
+        return new ReviewEntity(content, rating, userId, recommendationId);
+    }
 }
