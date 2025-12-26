237b8d9496e40a01e8284f1f8d0ec14819e3f820
이용우 <qhtm008@naver.com>
2025-12-26 14:08:50 +0900

    feaãixview): 후서비ì후ê¸ã후기 서비스 수정

--- backend/src/main/java/com/mukkebi/foodfinder/core/api/controller/v1/response/ReviewResponse.java
+++ backend/src/main/java/com/mukkebi/foodfinder/core/api/controller/v1/response/ReviewResponse.java
@@ -1,20 +1,7 @@
 package com.mukkebi.foodfinder.core.api.controller.v1.response;
-
-
-import com.mukkebi.foodfinder.core.domain.Review;
-
 public record ReviewResponse(
         String content,
         Double rating,
-        Long userId,
+        String userNickname,
         Long restaurantId
-) {
-    public static ReviewResponse from(Review r) {
-        return new ReviewResponse(
-                r.getContent(),
-                r.getRating(),
-                r.getUserId(),
-                r.getRestaurantId()
-        );
-    }
-}
+) {}
--- backend/src/main/java/com/mukkebi/foodfinder/storage/ReviewRepository.java
+++ backend/src/main/java/com/mukkebi/foodfinder/storage/ReviewRepository.java
@@ -1,12 +1,56 @@
 package com.mukkebi.foodfinder.storage;
 
 import com.mukkebi.foodfinder.core.domain.Review;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByUserId(Long userId);
-    List<Review> findByRestaurantId(Long restaurantId);
+
+//    List<Review> findTop20ByRestaurantIdOrderByCreatedAtDesc(Long restaurantId);
+//
+//    List<Review> findTop20ByUserIdOrderByCreatedAtDesc(Long userId);
+
+    @Query("""
+        select avg(r.rating)
+        from Review r
+        where r.restaurantId = :restaurantId
+    """)
+    Double findAverageRatingByRestaurantId(@Param("restaurantId") Long restaurantId);
+
+    @Query("""
+        select avg(r.rating)
+        from Review r
+        where r.userId = :userId
+    """)
+    Double findAverageRatingByUserId(@Param("userId") Long userId);
+
+    @Query("""
+    select r
+    from Review r
+    where r.restaurantId = :restaurantId
+      and (:cursorId is null or r.id < :cursorId)
+    order by r.id desc
+""")
+    List<Review> findByRestaurantWithCursor(
+            @Param("restaurantId") Long restaurantId,
+            @Param("cursorId") Long cursorId,
+            Pageable pageable
+    );
+
+    @Query("""
+    select r
+    from Review r
+    where r.userId = :userId
+      and (:cursorId is null or r.id < :cursorId)
+    order by r.id desc
+""")
+    List<Review> findByUserWithCursor(
+            @Param("userId") Long userId,
+            @Param("cursorId") Long cursorId,
+            Pageable pageable
+    );
 }
